99ac507b8e5e5b4e97e2b5d0c04e87f903c90bb5
captainabdully <abdulahmad0305@gmail.com>
2025-10-08 16:01:50 +0300

    first commit

---
+++ Main.py
@@ -0,0 +1,26 @@
+# Creating an empty list
+my_list = []
+
+# Append the element
+my_list.append(10),
+my_list.append(20),
+my_list.append(30),
+my_list.append(40),
+
+#Inserting 15 value at 2nd position
+my_list.insert(1,15)
+
+#Entending the list [50,60,70]
+my_list.extend([50,60,70])
+
+#removing the last item
+my_list.pop()
+
+#Sorting in ascending order
+my_list.sort()
+
+#Printing the index value of  30
+index_30 = my_list.index(30)
+
+print(index_30)
+
